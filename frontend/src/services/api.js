dc5d87fcd4d754d1a2c2c13f872480e4d82fc2c6
Prakruti25 <prakrutipatel256@gmail.com>
2026-05-24 15:20:09 -0400

    feat: connect frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://127.0.0.1:5001/api' });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:5001/api'
+});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
